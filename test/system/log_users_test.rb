d402f86543e220c0fb4e405402facaf7996bd396
Alex Platteeuw <alexplatteeuw@gmail.com>
2023-07-29 11:25:20 +0200

    test user login

---
+++ test/system/log_users_test.rb
@@ -0,0 +1,27 @@
+require "application_system_test_case"
+
+class LogUsersTest < ApplicationSystemTestCase
+  test "log in a user" do
+    OmniAuth.config.test_mode = true
+    
+    auth_data = {
+      uid: '12345', 
+      info: { email: 'test@example.com', name: 'John Doe' }, 
+      credentials: { token: 'ghu_1eX0SEMtRA66t0d7UHtktZ8LkzcPiI2MRls5' }
+    }
+
+    OmniAuth.config.add_mock(:github, **auth_data)
+
+    visit sessions_new_path
+
+    click_button "Login with GitHub"
+
+    assert_current_path '/welcome.html'
+
+    user = User.last
+
+    assert_equal 'John Doe', user.name
+    assert_equal '12345', user.github_uid
+    assert_equal 'test@example.com', user.email
+  end
+end
