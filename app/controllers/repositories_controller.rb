8d1f244ab26b591c9af2a098fc82182027007597
Alex Platteeuw <alexplatteeuw@gmail.com>
2023-07-29 15:43:04 +0200

    add basic search

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,15 @@
 class RepositoriesController < ApplicationController
   def index
-    @repositories = github_client.search_repositories("language:ruby").items
+    @repositories = github_client.search_repositories(terms).items
   end
 
   private
 
   def github_client
     Octokit::Client.new(per_page: 20)
+  end
+
+  def terms
+    @terms = params[:terms].presence || "rails"
   end
 end
