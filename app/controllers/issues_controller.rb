a3be36b7cbcb2f32aea769d0c70391e44d4e611e
Alexandre Ruban <alexandre@hey.com>
2023-07-29 16:27:46 +0200

    Show issue

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,6 +4,10 @@ class IssuesController < ApplicationController
     @issues = github_client.issues(repository_name, sort: { updated: :desc })
   end
 
+  def show
+    @issue = github_client.issue(repository_name, params[:id])
+  end
+
   private
 
   def repository_name
@@ -13,5 +17,4 @@ class IssuesController < ApplicationController
   def github_client
     Octokit::Client.new(per_page: 20)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get "auth/github/callback", to: 'oauth_callbacks#github'
-
   root to: 'sessions#new'
 
+  get "auth/github/callback", to: 'oauth_callbacks#github'
+
   resources :repositories, only: [:index, :show], param: :name do
-    resources :issues, only: [:index]
+    resources :issues, only: [:index, :show]
   end
 end
