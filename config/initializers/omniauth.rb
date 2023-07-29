6365e4256a010fc2eec0f85ceedb42ae55fc5cdd
Alex Platteeuw <alexplatteeuw@gmail.com>
2023-07-29 08:48:42 +0200

    ensure compatibility with rack_csrf

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 OmniAuth.config.logger = Rails.logger
+
+OmniAuth::AuthenticityTokenProtection.default_options(key: "csrf.token", authenticity_param: "_csrf")
