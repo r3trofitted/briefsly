8db7b7df4ab8cb27403eca9f3b4f4837246d3219
Alex Platteeuw <alexplatteeuw@gmail.com>
2023-07-30 12:22:47 +0200

    add profile basis

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,8 +1,15 @@
 class ProfileController < ApplicationController
   def edit
-    @user = Current.user
+    @user = user
   end
   
   def update
   end
+
+  private
+
+  def user
+    @_user ||= Octokit.user(Current.user.github_nickname)
+  end
+
 end
