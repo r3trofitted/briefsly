684b68a94edab1f87e79660e345967ed6fb3de71
Alex Platteeuw <alexplatteeuw@gmail.com>
2023-07-30 14:58:54 +0200

    level rating

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,9 +4,20 @@ class ProfileController < ApplicationController
   end
   
   def update
+    if Current.user.update(experience_level: experience_level)
+      redirect_to edit_profile_path
+    else
+      flash[:alert] = "Something went wrong."
+      @user = user
+      render :edit
+    end
   end
 
   private
+
+  def experience_level
+    params[:user][:experience_level].to_i
+  end
 
   def user
     @_user ||= Octokit.user(Current.user.github_nickname)
