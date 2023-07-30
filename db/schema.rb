3e464068b5eabc9fbdff107ca8d4c233ea98a9b0
Alex Platteeuw <alexplatteeuw@gmail.com>
2023-07-30 09:54:06 +0200

    remove not null constraint on column emain from table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_160301) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_30_075312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_160301) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "email", null: false
+    t.string "email"
     t.string "github_uid", null: false
     t.string "github_access_token"
     t.string "github_nickname", null: false
