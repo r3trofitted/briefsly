68de9c5eb9b2aff52e27542ed3dbbe5e2ae5f72d
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-07-30 11:09:18 +0200

    Moved the slot search to a Search model
    This makes filling the form after a search easier. Code is not a
    funky has it could, but we're in a hurry!

---
+++ app/models/slot_search.rb
@@ -0,0 +1,31 @@
+class SlotSearch
+  extend ActiveModel::Naming
+  
+  def initialize(params)
+    @params = params.compact_blank
+  end
+  
+  def buddy_name
+    @params[:buddy_name]
+  end
+  
+  def issue_number
+    @params[:issue_number]
+  end
+  
+  def repository_name
+    @params[:repository_name]
+  end
+  
+  def slots
+    slots = Slot.includes(:user).all
+
+    slots = slots.by_repository(repository_name) if repository_name.present?
+    slots = slots.by_issue(issue_number) if issue_number.present?
+    slots = slots.references(:user).merge(User.by_buddyname(buddy_name)) if buddy_name.present?
+
+    slots.limit(48)
+    
+    slots
+  end
+end
